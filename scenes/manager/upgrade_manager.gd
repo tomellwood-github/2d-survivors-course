a6eb7b1da60ba14f565158ad80644879f4f8e259
Tom <ellwood2k7@gmail.com>
2023-09-02 19:33:53 +0100

    Added hit flash component, added more weapon upgrades

--- scenes/manager/upgrade_manager.gd
+++ scenes/manager/upgrade_manager.gd
@@ -1,12 +1,22 @@
 extends Node
 
-@export var upgrade_pool: Array[AbilityUpgrade]
 @export var experience_manager: Node
 @export var upgrade_screen_scene: PackedScene
 
 var current_upgrades = {}
+var upgrade_pool: WeightedTable = WeightedTable.new()
+
+var upgrade_axe = preload("res://resources/upgardes/axe.tres")
+var upgrade_axe_damage = preload("res://resources/upgardes/axe_damage.tres")
+var upgrade_sword_rate = preload("res://resources/upgardes/sword_rate.tres")
+var upgrade_sword_damage = preload("res://resources/upgardes/sword_damage.tres")
+
 
 func _ready():
+	upgrade_pool.add_item(upgrade_axe, 10)
+	upgrade_pool.add_item(upgrade_sword_damage, 10)
+	upgrade_pool.add_item(upgrade_sword_rate, 10)
+
 	experience_manager.level_up.connect(on_level_up)
 
 
@@ -20,24 +30,27 @@ func apply_upgrade(upgrade: AbilityUpgrade):
 	else:
 		current_upgrades[upgrade.id]["quantity"] += 1
 
-	if current_upgrades[upgrade.id]["quantity"] >= upgrade.max_quantity:
-		upgrade_pool = upgrade_pool.filter(func(_upgrade): return _upgrade.id != upgrade.id)
+	if upgrade.max_quantity > 0 && current_upgrades[upgrade.id]["quantity"] >= upgrade.max_quantity:
+		upgrade_pool.remove_item(upgrade)
+
+	update_upgrade_pool(upgrade)
 
 	GameEvents.emit_ability_upgrade_added(upgrade, current_upgrades)
 
 
+func update_upgrade_pool(chosen_upgrade: AbilityUpgrade):
+	if chosen_upgrade.id == upgrade_axe.id:
+		upgrade_pool.add_item(upgrade_axe_damage, 10)
+
+
 func pick_upgrades():
 	var chosen_upgrades: Array[AbilityUpgrade] = []
-	# Any need to duplicate if filter returns a new array?
-	var filtered_upgrades = upgrade_pool.duplicate()
 	for i in 2:
-		if filtered_upgrades.size() == 0:
+		if upgrade_pool.items.size() == chosen_upgrades.size():
 			break
 
-		var chosen_upgrade = filtered_upgrades.pick_random() as AbilityUpgrade
+		var chosen_upgrade = upgrade_pool.pick_item(chosen_upgrades)
 		chosen_upgrades.append(chosen_upgrade)
-		# Any need to filter here if we could shuffle the array and just pick first?
-		filtered_upgrades = filtered_upgrades.filter(func (upgrade): return upgrade.id != chosen_upgrade.id)
 
 	return chosen_upgrades
 
--- scripts/weighted_table.gd
+++ scripts/weighted_table.gd
@@ -9,11 +9,30 @@ func add_item(item, weight: int):
 	weight_sum += weight
 
 
-func pick_item():
-	var chosen_weight = randi_range(1, weight_sum)
+func remove_item(item):
+	items = items.filter(func (_item): return _item["item"] != item)
+	weight_sum = 0
+	for _item in items:
+		weight_sum += _item["weight"]
+
+
+func pick_item(exclude: Array = []):
+	var adjusted_items: Array[Dictionary] = items
+	var adjusted_weight_sum = weight_sum
+
+	if exclude.size() > 0:
+		adjusted_items = []
+		adjusted_weight_sum = 0
+		for item in items:
+			if item["item"] in exclude:
+				continue
+			adjusted_items.append(item)
+			adjusted_weight_sum += item["weight"]
+
+	var chosen_weight = randi_range(1, adjusted_weight_sum)
 	var iteration_sum = 0
 
-	for item in items:
+	for item in adjusted_items:
 		iteration_sum += item["weight"]
 		if chosen_weight <= iteration_sum:
 			return item["item"]

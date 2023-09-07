57aae148b4cca19e8affed931844defa713b979e
Tom <ellwood2k7@gmail.com>
2023-09-07 09:01:35 +0100

    Add save/load system

--- scenes/autoload/meta_progression.gd
+++ scenes/autoload/meta_progression.gd
@@ -1,5 +1,7 @@
 extends Node
 
+const SAVE_FILE_PATH = "user://game.save"
+
 var save_data: Dictionary = {
 	"win_count": 0,
 	"loss_count": 0,
@@ -10,6 +12,21 @@ var save_data: Dictionary = {
 
 func _ready():
 	GameEvents.experience_vial_collected.connect(on_experience_collected)
+	load_save_file()
+
+
+func load_save_file():
+	if not FileAccess.file_exists(SAVE_FILE_PATH):
+		return
+
+	var file = FileAccess.open(SAVE_FILE_PATH, FileAccess.READ)
+	save_data = file.get_var()
+	print(save_data)
+
+
+func save():
+	var file = FileAccess.open(SAVE_FILE_PATH, FileAccess.WRITE)
+	file.store_var(save_data)
 
 
 func add_meta_upgrade(upgrade: MetaUpgrade):
